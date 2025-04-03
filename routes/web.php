12ebfa4bbd7a13ab2b45a1fb58f37958694f4a6f
Ars-Dev <arsenapresyan.dev@gmail.com>
2025-04-03 12:21:55 +0200

    putu logout in group

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ Route::get('/products/{product_id}', [ProductController::class, 'show'])->name('
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/admin/products', [AdminController::class, 'products'])->name('admin.products');
@@ -20,4 +19,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/admin/products/edit/{id}', [AdminController::class, 'editProduct'])->name('admin.edit.product');
     Route::post('/admin/products/edit/{id}', [AdminController::class, 'updateProduct'])->name('admin.update.product');
     Route::get('/admin/products/delete/{id}', [AdminController::class, 'deleteProduct'])->name('admin.delete.product');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });
